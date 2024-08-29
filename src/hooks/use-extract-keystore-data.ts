9173141c687f51aa678f21c17d28c51b9a1b7202
Sumbat <sumbat@ssvlabs.io>
2024-08-29 13:10:13 +0300

    removed worker

--- src/hooks/use-extract-keystore-data.ts
+++ src/hooks/use-extract-keystore-data.ts
@@ -1,31 +1,32 @@
 import type { MutationConfig } from "@/lib/react-query";
-import type { KeystoreResponseMessage } from "@/workers/extract-keystore-data";
 import { useMutation } from "@tanstack/react-query";
 
-import ExtractKeystoreDataWorker from "@/workers/extract-keystore-data?worker&inline";
-console.log("ExtractKeystoreDataWorker:", ExtractKeystoreDataWorker);
-import type { ExtractedKeys } from "ssv-keys/dist/tsc/src/lib/SSVKeys";
-const worker = new ExtractKeystoreDataWorker();
+// import ExtractKeystoreDataWorker from "@/workers/extract-keystore-data?worker&url";
+// const worker = new Worker(ExtractKeystoreDataWorker);
+// console.log("ExtractKeystoreDataWorker:", ExtractKeystoreDataWorker);
+import { SSVKeys, type ExtractedKeys } from "ssv-keys/dist/tsc/src/lib/SSVKeys";
 
 type Params = {
   file: File;
   password: string;
 };
 
+const ssvKeys = new SSVKeys();
+
 export const extractKeys = async ({
   file,
   password,
 }: Params): Promise<ExtractedKeys> => {
-  console.log("worker:", worker);
-  return new Promise((resolve, reject) => {
-    worker.onmessage = (e: KeystoreResponseMessage) => {
-      const { data, error } = e.data;
-      console.log("data:", data);
-      return error ? reject(error) : resolve(data);
-    };
-    console.log("worker.postMessage:", worker.postMessage);
-    worker.postMessage({ file, password });
-  });
+  const text = await file.text();
+  return await ssvKeys.extractKeys(text, password);
+  // return new Promise((resolve, reject) => {
+  //   worker.onmessage = (e: KeystoreResponseMessage) => {
+  //     const { data, error } = e.data;
+  //     console.log("data:", data);
+  //     return error ? reject(error) : resolve(data);
+  //   };
+  //   worker.postMessage({ file, password });
+  // });
 };
 
 export const useExtractKeystoreData = (
