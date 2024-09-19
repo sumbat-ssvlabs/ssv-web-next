5838639c47b7ef2caebda742eebf6002827dbec4
sumbat-ssvlabs <sumbat@ssvlabs.io>
2024-09-19 15:41:18 +0300

    fix: 5

--- release.config.cjs
+++ release.config.cjs
@@ -7,7 +7,14 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false, // Do not publish to npm
+        pkgRoot: ".", // Path to package.json
+      },
+    ],
     "@semantic-release/git",
+    "@semantic-release/github",
   ],
 };
