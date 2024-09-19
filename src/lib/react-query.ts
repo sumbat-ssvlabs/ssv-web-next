87b13fa1ecefa8ab73d71841a58ba54b823a3695
sumbat-ssvlabs <sumbat@ssvlabs.io>
2024-09-19 16:10:50 +0300

    fix: testing 3

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -18,6 +18,7 @@ const storageKey = "ssv:query-client-offline-cache";
 const versionKey = "ssv:web-app-version";
 
 const prevVersion = window.localStorage.getItem(versionKey);
+console.log("prevVersion:", prevVersion);
 if (prevVersion !== APP_VERSION) {
   window.localStorage.removeItem(storageKey);
   window.localStorage.setItem(versionKey, APP_VERSION);
